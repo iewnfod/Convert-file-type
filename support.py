c53c2ca4519c0a4794c50f9c111dae8930971871
Iewnfod <shangzhiqiuyue@outlook.com>
2023-03-04 15:46:46 +0800

    remove restart

--- support.py
+++ support.py
@@ -6,7 +6,3 @@ def unzip(zip_path, save_path):
     file = zipfile.ZipFile(zip_path)
     file.extractall(save_path)
     file.close()
-
-def restart():
-    python = sys.executable
-    os.execl(python, python, * sys.argv)
