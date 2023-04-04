c93e9f7f517d7aa5880389f6f7f72be5f316e8ec
Iewnfod <shangzhiqiuyue@outlook.com>
2023-04-04 15:09:31 +0800

    Fix ffmpeg install

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,9 @@
-from src.main import main
 from src.constants import *
 
-
 def __init__():
+    print('Initializing')
     # init
     initialize_ffmpeg()
     # main
+    from src.main import main
     main()
