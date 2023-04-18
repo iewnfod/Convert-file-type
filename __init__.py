6ac1ba36956e9d9bd9516e03b953b6d78d5ab889
Iewnfod <shangzhiqiuyue@outlook.com>
2023-04-18 19:46:44 +0800

    Prevent reinstall of libs

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import pip
+import importlib
 
 # 目录设置
 if os.path.splitext(sys.path[0])[1] == '.zip':
@@ -9,8 +10,19 @@ if os.path.splitext(sys.path[0])[1] == '.zip':
 print(f'当前运行目录: {os.getcwd()}')
 
 # 安装所需库
-libs = ['PySide6', 'pypandoc', 'wget', 'moviepy', 'pillow']
-pip.main(['install', *libs, '-i', 'https://pypi.tuna.tsinghua.edu.cn/simple'])
+# (包名，导入名)
+libs = [
+    ('PySide6', 'PySide6'),
+    ('pypandoc', 'pypandoc'),
+    ('wget', 'wget'),
+    ('moviepy', 'moviepy'),
+    ('pillow', 'PIL')
+]
+for lib_name, import_name in libs:
+    try:
+        importlib.import_module(import_name)
+    except:
+        pip.main(['install', lib_name, '-i', 'https://pypi.tuna.tsinghua.edu.cn/simple'])
 
 # 启动
 import src
