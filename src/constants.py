8f637125ddeda4f02ff74f56ef1a491a4a4c0991
Iewnfod <shangzhiqiuyue@outlook.com>
2023-04-25 18:20:49 +0800

    Check if ffmpeg is in PATH

--- src/constants.py
+++ src/constants.py
@@ -5,9 +5,12 @@ import pypandoc
 import ssl
 import wget
 
+
+# 初始化
 PLATFORM_INFO = platform.uname()
 SYSTEM = PLATFORM_INFO.system
 ARCHITECTURE = PLATFORM_INFO.machine
+PATH = set()
 
 print(f'平台信息: \n\t系统: {SYSTEM}\n\t架构: {ARCHITECTURE}')
 
@@ -24,11 +27,15 @@ def initialize_ffmpeg():
     if SYSTEM == 'Darwin':
         os.system('chmod +x ffmpeg/MacOS/ffmpeg')  # 赋予 ffmpeg 可执行权限
         os.environ['PATH'] += ':' + os.path.join(os.getcwd(), 'ffmpeg/MacOS')
+    elif SYSTEM == 'win32' or SYSTEM == 'Windows':
+        if 'ffmpeg.exe' not in PATH or 'ffmpeg' not in PATH:
+            print('You should install ffmpeg by yourself. ')
+            os.system('explorer https://github.com/BtbN/FFmpeg-Builds/releases')
 
 
 min_width = float('inf')
 
-def draw_bar(current, total, width=80):
+def _draw_bar(current, total, width=80):
     global min_width
     if current == -1 or total == -1:
         return
@@ -51,7 +58,7 @@ def install_pandoc():
     if SYSTEM == 'Darwin':
         name = url['darwin'].split('/')[-1]
         if not os.path.exists(name):
-            wget.download(url['darwin'], bar=draw_bar)
+            wget.download(url['darwin'], bar=_draw_bar)
             print()
         os.system(f'open \'{name}\'')
 
@@ -69,7 +76,10 @@ def install_pandoc():
 
 def getPATH():
     path = set()
-    for i in os.environ['PATH'].split(':'):
+    splitter = ':'
+    if SYSTEM == 'win32' or SYSTEM == 'Windows':
+        splitter = ';'
+    for i in os.environ['PATH'].split(splitter):
         if not os.path.exists(i):
             continue
         files = os.listdir(i)
@@ -89,12 +99,14 @@ def initialize_pandoc():
     print('\033[1mInitializing pandoc\033[0m')
     # 如果 pandoc 不存在，或者没有，就下载安装
     try:
-        if pypandoc.get_pandoc_path() not in getPATH():
+        if pypandoc.get_pandoc_path() not in PATH:
             install_pandoc()
     except:
         install_pandoc()
 
 
 def __init__():
+    global PATH
+    PATH = getPATH()
     initialize_ffmpeg()
     initialize_pandoc()
